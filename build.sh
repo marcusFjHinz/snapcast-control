9632c2fd4d51edcc53177bb307f7eb1170356cdb
Marcus Hinuz <marcus.fj.hinz@gmail.com>
2022-11-12 19:56:43 +0100

    add some echoes

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
+echo START build DONE
+echo cleaning up ...
 rm -rf ./build
 mkdir -p ./build/yascui/snappy
 
+echo running npm install
 npm install
+echo running npm build
 npm run build
 
+echo copy files
 cp -r ./dist/* ./build/yascui/snappy
 cp -r ./yascui_service/* ./build/yascui
-
+echo build DONE!
